e47f3401d8249cac13772e5980f9364d79e784e6
[Nate] <[ncggardner@gmail.com]>
2020-05-27 22:55:57 -0400

    linq first commit

---
+++ LinqLab/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 2330, 112233, 10, 949, 3764, 2942 };
+
+            var smallest = nums.Min();
+            Console.WriteLine("Smallest Number: {0}", smallest);
+
+            var largest = nums.Max();
+            Console.WriteLine("Largest Number: {0}", largest);
+
+           foreach(int i in nums)
+            {
+                if (i < 10000)
+                {
+                    var biggest = nums.Max();
+                    Console.WriteLine("Largest Number under 10000: {0}", biggest);
+                }
+            }
+
+            foreach (int i in nums)
+            {
+                if (i < 100)
+                {
+                    if (i > 10)
+                    {                        
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+
+            foreach (int i in nums)
+            {
+                if (i <= 999999)
+                {
+                    if (i >= 100000)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+            var evenElements = nums.Count(i => i % 2 == 0);
+
+            Console.WriteLine("Even Numbers: {0}", evenElements);
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Jimmy", 13));
+            students.Add(new Student("Hannah Banana", 21));
+            students.Add(new Student("Justin", 30));
+            students.Add(new Student("Sarah", 53));
+            students.Add(new Student("Hannibal", 15));
+            students.Add(new Student("Phillip", 16));
+            students.Add(new Student("Maria", 63));
+            students.Add(new Student("Abe", 33));
+            students.Add(new Student("Curtis", 10));
+
+            Solution s = new Solution(students);
+            s.FindStudentsOver21();
+            s.FindStudentsOver21AndEven();
+            s.FindTeanageStudents();
+
+
+            var oldest = students.Max(s => s.Age);
+            var result = from c in students
+                         where c.Age == oldest
+                         select c;
+
+            foreach (Student a in result)
+            {
+                Console.WriteLine(a.Name);
+            }
+
+            var youngest = students.Min(s => s.Age);
+            var otherResult = from c in students
+                         where c.Age == youngest
+                         select c;
+
+            foreach (Student a in otherResult)
+            {
+                Console.WriteLine(a.Name);
+            }
+
+            var vowelNames = from v in students
+                             where v.Name.ToLower().StartsWith("a")                             
+                             select v;
+
+            foreach (Student a in vowelNames)
+            {
+                Console.WriteLine(a.Name);
+            }
+        }
+    }
+}
---
+++ LinqLab/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinqLab
+{
+    class Solution
+    {
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public Solution(List<Student> testData)
+        {
+            this.Students = testData;
+        }
+
+        
+       
+
+        public List<Student> FindStudentsOver21()
+        {
+            List<Student> output = new List<Student>();
+            output = Students.Where(s => s.Age >= 21).ToList();
+
+            PrintStudents(output);
+            return output;
+        }        
+
+        public List<Student> FindStudentsOver21AndEven()
+        {
+            List<Student> output = new List<Student>();
+            output = Students.Where(s => s.Age >= 21 && s.Age % 2 == 0).ToList();
+
+            PrintStudents(output);
+            return output;
+        }
+
+        public List<Student> FindTeanageStudents()
+        {
+            List<Student> output = new List<Student>();
+            foreach (Student s in Students)
+            {
+                if (s.Age >= 13)
+                {
+                    if (s.Age <= 19)
+                    {
+                        output.Add(s);
+                    }
+                }
+            }
+            PrintStudents(output);
+            return output;
+        }
+
+        public void PrintStudents(List<Student> input)
+        {
+            foreach (Student s in input)
+            {
+                Console.WriteLine("Name: " + s.Name);
+                Console.WriteLine("Age: " + s.Age);
+                Console.WriteLine();
+            }
+        }
+    }
+}
